ac4d8256233c5ed80d181de92119ef2495c86dfe
Yaroslav Maruhno <81463618+maryaroslav@users.noreply.github.com>
2021-04-30 02:30:18 +0300

    jscript

---
+++ lang.js
@@ -0,0 +1,204 @@
+const langArr = {
+    "home": {
+        "ru": "Домашняя",
+        "en": "Home",
+    },
+    
+    "assembly-one": {
+        "ru": "Сборка №1",
+        "en": "Assembly №1",
+    },
+    
+    "assembly-two": {
+        "ru": "Сборка №2",
+        "en": "Assembly №2",
+    },
+
+    "about": {
+        "ru": "Про нас",
+        "en": "About Us",
+    },
+
+    "description": {
+        "ru": "Описание компании",
+        "en": "Company sescription",
+    },
+    
+    "services": {
+        "ru": "Мы предоставляем хорошие и оптимизированные сборки ПК, так же на сайте вы можете купить отдельно комплектующие и периферию.",
+        "en": "We provide good and optimized PC builds, as well as on the website you can buy separately components and peripherals.",
+    },
+
+    "stock": {
+        "ru": "В наличии:",
+        "en": "In stock:",
+    },
+
+    "ready": {
+        "ru": "Готовые сборки",
+        "en": "Assembbled PC",
+    },
+
+    "accessor": {
+        "ru": "Комплектующие",
+        "en": "Accesserories",
+    },
+
+    "periphery": {
+        "ru": "Периферия",
+        "en": "Periphery",
+    },
+
+    "name-pc-one" : {
+        "ru": "Компьютер",
+        "en": "Computer",
+    },
+
+    "name-pc-two" : {
+        "ru": "Компьютер",
+        "en": "Computer",
+    },
+
+    "charact": {
+        "ru": "Характеристики",
+        "en": "Characterisics",
+    },
+
+    "cpu": {
+        "ru": "Процессор",
+        "en": "CPU",
+    },
+
+    "mothboard": {
+        "ru": "Материинская плата",
+        "en": "Mother Board",
+    },
+    
+    "ram": {
+        "ru": "Оперативная память",
+        "en": "RAM",
+    },
+
+    "ram-intel": {
+        "ru": "16 Гб DDR4-2666 MHz",
+        "en": "16 Gb DDR4-2666 MHz",
+    },
+
+    "card": {
+        "ru": "Видеокарта",
+        "en": "Graphics Card",
+    },
+
+    "hdd": {
+        "ru": "Жесткий диск",
+        "en": "HDD",
+    },
+
+    "hdd-intel": {
+        "ru": "1000 Гб SATA III",
+        "en": "1000 Gb SATA III",
+    },
+
+    "ssd-intel": {
+        "ru": "240 Гб",
+        "en": "240 Gb",
+    },
+
+    "power": {
+        "ru": "Блок питания",
+        "en": "Power supply",
+    },
+
+    "power-intel": {
+        "ru": "500 Вт",
+        "en": "500 Wt"
+    },
+
+    "case": {
+        "ru": "Корпус",
+        "en": "Сase",
+    },
+
+    "guar": {
+        "ru": "Гарантия кол-во мес.",
+        "en": "Guarantee mo.",
+    },
+
+    "guar-intel": {
+        "ru": "до 24 мес.",
+        "en": "up to 24 mo.",
+    },
+
+
+
+    "charact-amd": {
+        "ru": "Характеристики",
+        "en": "Characterisics",
+    },
+    
+    "cpu-cobra": {
+        "ru": "Процессор",
+        "en": "CPU",
+    },
+
+    "mothboard-cobra": {
+        "ru": "Материинская плата",
+        "en": "Mother Board",
+    },
+
+    "ram-cobra": {
+        "ru": "Оперативная память",
+        "en": "RAM",
+    },
+
+    "card-cobra": {
+        "ru": "Видеокарта",
+        "en": "Graphics Card",
+    },
+
+    "hdd-cobra": {
+        "ru": "Жесткий диск",
+        "en": "HDD",
+    },
+
+    "power-cobra": {
+        "ru": "Блок питания",
+        "en": "Power supply",
+    },
+
+    "case-cobra": {
+        "ru": "Корпус",
+        "en": "Сase",
+    },
+
+    "guar-cobra": {
+        "ru": "Гарантия кол-во мес.",
+        "en": "Guarantee mo.",
+    },
+
+    "ram-amd": {
+        "ru": "8 Гб DDR4-2666 MHz",
+        "en": "8 Gb DDR4-2666 MHz",
+    },
+
+    "hdd-amd": {
+        "ru": "1000 Гб SATA III",
+        "en": "1000 Gb SATA III",
+    },
+
+    "ssd-amd": {
+        "ru": "240 Гб",
+        "en": "240 Gb",
+    },
+
+    "power-amd": {
+        "ru": "500 Вт",
+        "en": "500 Wt"
+    },
+    
+    "guar-amd": {
+        "ru": "до 36 мес.",
+        "en": "up to 36 mo.",
+    },
+
+}
